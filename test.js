8067f22cd59edb21774020f22134f303b5e1c5a5
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-02 16:57:13 +0000

    adjusted the input value

--- test.js
+++ test.js
@@ -11,9 +11,9 @@ test("One euro should be 1.07 dollars", function() {
 
     const { fromEuroToDollar } = require('./app.js');
 
-    const dollars = fromEuroToDollar(3.5);
+    const dollars = fromEuroToDollar(2);
 
-    const expected = 3.5 * 1.07; 
+    const expected = 2 * 1.07; 
     
      expect(fromEuroToDollar(2)).toBe(2.14);
 })
@@ -24,7 +24,7 @@ test("One dollar should be 146.26168224299064 yens", function() {
 
     const yens = fromDollarToYen(2);
 
-    const expected = 3.5 * 156.5 / 1.07; 
+    const expected = 2 * 156.5 / 1.07; 
     
      expect(fromDollarToYen(2)).toBe(292.5233644859813);
 })
@@ -35,7 +35,7 @@ test("One yen should be 0.005559105431309904 pounds", function() {
 
     const pounds = fromYenToPound(2);
 
-    const expected = 3.5 * 0.87 / 156.6; 
+    const expected = 2 * 0.87 / 156.6; 
     
      expect(fromYenToPound(2)).toBe(0.011118210862619809);
 })
